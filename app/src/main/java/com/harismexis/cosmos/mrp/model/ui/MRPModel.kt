e3cf8740b5ba382b58cdc054335f4d4f9237dbe9
Haris Mexis <theoharismexis@hotmail.com>
2021-04-10 12:29:50 +0100

    update

--- app/src/main/java/com/harismexis/cosmos/mrp/model/ui/MRPModel.kt
+++
@@ -1,8 +0,0 @@
-package com.harismexis.cosmos.mrp.model.ui
-
-/**
- * Model for an MRP response containing a list of Mars Rover Photo Items
- */
-data class MRPModel(
-    var mrpItems: MutableList<MRPItemModel>
-)
