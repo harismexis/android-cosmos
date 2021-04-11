cc20e5a7423b12c05b8338ea945657a5b914e871
Haris Mexis <theoharismexis@hotmail.com>
2021-04-11 10:49:50 +0100

    updates

--- app/src/main/java/com/harismexis/cosmos/niavl/viewholder/NIAVLItemVh.kt
+++ app/src/main/java/com/harismexis/cosmos/niavl/viewholder/NIAVLItemVh.kt
@@ -22,7 +22,7 @@ class NIAVLItemVh(
     ) {
         itemView.context.populateWithGlide(binding.imgv, item.thumbUrl)
         setupPlayIcon(item.mediaType == NIAVLDataEntry.MEDIA_TYPE_VIDEO)
-        binding.txt1.text = item.description
+        binding.txt1.text = item.title
         binding.root.setOnClickListener {
             itemClickListener.onNIAVLItemClick(item, position)
         }
